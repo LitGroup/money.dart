09a55e3c195dc329fac7334380c6146eb8d2eed1
Roman Shamritskiy <roman@litgroup.ru>
2015-05-05 16:47:33 +0300

    Restores test/all.dart to run coveralls tool.

---
+++ test/all.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, the package authors. Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// MIT license that can be found in the LICENSE file.
+
+import 'currency_test.dart' as currency_test;
+import 'money_test.dart' as money_test;
+
+void main() {
+  currency_test.main();
+  money_test.main();
+}
