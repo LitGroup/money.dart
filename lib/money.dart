e6d41f0701ac05f782ea1d088f09c7c5cacf5b6c
Roman Shamritskiy <roman@litgroup.ru>
2016-08-15 13:37:09 +0300

    Changes name of library from "ru.litgroup.money" to "money".

--- lib/money.dart
+++ lib/money.dart
@@ -2,7 +2,7 @@
 // This source file is subject to the MIT license that is bundled
 // with this source code in the file LICENSE.
 
-library ru.litgroup.money;
+library money;
 
 part 'src/currency.dart';
 part 'src/money.dart';
--- lib/src/currency.dart
+++ lib/src/currency.dart
@@ -2,7 +2,7 @@
 // This source file is subject to the MIT license that is bundled
 // with this source code in the file LICENSE.
 
-part of ru.litgroup.money;
+part of money;
 
 /// Currency Value Object.
 ///
