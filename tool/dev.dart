6d269146a7da4310c9cb8fa66c932ebe09ef26c4
Roman Shamritskiy <roman@litgroup.ru>
2016-09-28 15:21:12 +0300

    Update copyright. [ci skip]

--- tool/dev.dart
+++ tool/dev.dart
@@ -1,9 +1,3 @@
-// (c) 2016 Roman Shamritskiy <roman@litgroup.ru>
-// This source file is subject to the MIT license that is bundled
-// with this source code in the file LICENSE.
-
-library tool.dev;
-
 import 'package:dart_dev/dart_dev.dart' show dev, config;
 
 main(List<String> args) async {
@@ -20,6 +14,9 @@ main(List<String> args) async {
   config.coverage
     ..html = true
     ..output = 'build/coverage/';
+
+  config.copyLicense
+    ..directories = const['lib/', 'test/', 'example/'];
 
   await dev(args);
 }
