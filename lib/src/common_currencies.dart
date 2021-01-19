8c2b633c58fcc805644e380ca3ee4d1b20538fe9
Brett Sutton <bsutton@noojee.com.au>
2021-01-19 13:16:50 +1100

    Added bitcoin, sorted entries.

--- lib/src/common_currencies.dart
+++ lib/src/common_currencies.dart
@@ -24,23 +24,20 @@ class CommonCurrencies {
 
   CommonCurrencies._internal();
 
-  /// USA
-  final Currency usd = Currency.create('USD', 2);
-
   /// Australia
   final Currency aud = Currency.create('AUD', 2);
 
-  /// New Zealand
-  final Currency nzd = Currency.create('NZD', 2);
+  /// Brazilian Real
+  final Currency brl = Currency.create('BRL', 2, symbol: r'R$');
+
+  /// Bit coin dollar
+  final Currency btc = Currency.create('BTC', 8, symbol: '₿', pattern: 'S0');
 
   /// Canada
   final Currency cad = Currency.create('CAD', 2);
 
   /// Swiss Franc
   final Currency chf = Currency.create('CHF', 2, symbol: 'fr');
-
-  /// British Pound Sterling
-  final Currency gbp = Currency.create('GBP', 2, symbol: '£');
 
   /// Chinese renminbi
   final Currency cny = Currency.create('CNY', 2, symbol: '¥');
@@ -49,14 +46,15 @@ class CommonCurrencies {
   final Currency euro = Currency.create('EUR', 2,
       symbol: '€', invertSeparators: true, pattern: 'S0.000,00');
 
+  /// British Pound Sterling
+  final Currency gbp = Currency.create('GBP', 2, symbol: '£');
+
+  /// Indian Rupee
+  final Currency inr = Currency.create('INR', 2,
+      symbol: '₹', invertSeparators: true, pattern: 'S000.00,00');
+
   /// Japanese Yen
   final Currency jpy = Currency.create('JPY', 0, symbol: '¥', pattern: 'S0');
-
-  /// Mexican Peso
-  final Currency mxn = Currency.create('MXN', 2);
-
-  /// Norwegian krone
-  final Currency nok = Currency.create('NOK', 2, symbol: 'kr');
 
   /// South Korean Won
   final Currency krw = Currency.create('KRW', 0, symbol: '₩', pattern: 'S0');
@@ -64,15 +62,23 @@ class CommonCurrencies {
   /// Turkish Lira
   final Currency ltry = Currency.create('TRY', 2, symbol: '₺');
 
-  /// Indian Rupee
-  final Currency inr = Currency.create('INR', 2,
-      symbol: '₹', invertSeparators: true, pattern: 'S000.00,00');
+  /// Mexican Peso
+  final Currency mxn = Currency.create('MXN', 2);
+
+  /// Norwegian krone
+  final Currency nok = Currency.create('NOK', 2, symbol: 'kr');
+
+  /// New Zealand
+  final Currency nzd = Currency.create('NZD', 2);
 
   /// Russian Ruble
   final Currency rub = Currency.create('RUB', 2, symbol: '₽');
 
-  /// Brazilian Real
-  final Currency brl = Currency.create('BRL', 2, symbol: r'R$');
+  /// New Taiwan dollar
+  final Currency twd = Currency.create('TWD', 0, symbol: r'NT$', pattern: 'S0');
+
+  /// USA
+  final Currency usd = Currency.create('USD', 2);
 
   /// South African Rand
   final Currency zar = Currency.create('ZAR', 2, symbol: 'R');
@@ -80,22 +86,24 @@ class CommonCurrencies {
   /// Registers all of the common currency.
   void registerAll() {
     Currencies.registerList([
-      usd,
       aud,
-      nzd,
       cad,
       chf,
-      gbp,
+      brl,
+      btc,
       cny,
       euro,
+      inr,
+      gbp,
       jpy,
-      mxn,
-      nok,
       krw,
       ltry,
-      inr,
+      mxn,
+      nok,
+      nzd,
       rub,
-      brl,
+      twd,
+      usd,
       zar
     ]);
   }
