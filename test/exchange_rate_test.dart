143f9d562380fa77a1631add3d0a08802433a2a4
Brett Sutton <bsutton@noojee.com.au>
2021-01-19 13:17:14 +1100

    Added tests for exchange rate precision.

--- test/exchange_rate_test.dart
+++ test/exchange_rate_test.dart
@@ -12,5 +12,42 @@ void main() {
 
       expect(invoiceAmount.exchangeTo(auToUsExchangeRate), equals(us680));
     });
+
+    test('Exchange Rates - precision 1', () {
+      final Currency twd = Currency.create('TWD', 0, symbol: 'NT\$');
+      final Currency usd = Currency.create('USD', 2);
+
+      final Money twdM = Money.fromInt(1000, twd);
+      expect(twdM.toString(), equals(r'NT$1000.00'));
+
+      final twdToUsdRate = Money.fromInt(3, usd); // 1 TWD = 0.03 USD
+      expect(twdToUsdRate.toString(), equals(r'$0.03'));
+
+      final usdM = twdM.exchangeTo(twdToUsdRate);
+      expect(usdM.toString(), equals(r'$30.00'));
+
+      final Currency usdExchange = Currency.create('USD', 6);
+
+      final acurateTwdToUsdRate =
+          Money.fromInt(35231, usdExchange); // 1 TWD = 0.035 USD
+      expect(acurateTwdToUsdRate.toString(), equals(r'$0.035231'));
+
+      final usdMaccurate = twdM.exchangeTo(acurateTwdToUsdRate);
+      expect(usdMaccurate.toString(), equals(r'$35.23'));
+    });
+
+    test('Exchange Rates - precision 2', () {
+      final jpy = Currency.create('JPY', 0, symbol: '¥');
+      final twd = Currency.create('TWD', 0, symbol: 'NT\$');
+
+      final twdM = Money.fromInt(1000, twd);
+      expect(twdM.toString(), equals(r'NT$1000.00'));
+
+      final twdToJpyRate = Money.fromInt(3, jpy);
+      expect(twdToJpyRate.toString(), equals('¥3.00'));
+
+      final jpyM = twdM.exchangeTo(twdToJpyRate);
+      expect(jpyM.toString(), equals('¥3000'));
+    });
   });
 }
