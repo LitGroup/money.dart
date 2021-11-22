f29ca2fc2fbfc1b574b31e99fa101d2fdf09189f
Brett Sutton <bsutton@noojee.com.au>
2021-11-22 21:19:09 +1100

    Changed ExchangePlatform to work with ExchangeRate changes.

--- lib/src/exchange_rates/exchange_platform.dart
+++ lib/src/exchange_rates/exchange_platform.dart
@@ -2,16 +2,32 @@ import '../money.dart';
 import 'exchange_rate.dart';
 
 typedef CodePair = String;
-typedef Code = String;
 
+/// The [ExchangePlatform] allows you to register a set of [ExchangeRate]
+/// which you can then use to do currency conversions.
+/// We allow multiple [ExchangePlatform]s to exist to reflect
+/// the fact that there multiple exchanges around the world.
+///
+/// To use an [ExchangePlatform] call register for each exchange rate.
+/// If you only provide an exchange rate in one direction (AUD -> USD)
+/// then it will calculate the inverse exchange rate if required.
+/// In the real world you will probably need to provide exchange rates
+/// in both directions as they are rarely the recipricol of each other.
 class ExchangePlatform {
-  final exchangeMap = <Code, ExchangeRateMap>{};
-  void registerExchangeRate(ExchangeRateMap exchangeRate) {
-    exchangeMap[_generate(exchangeRate.from.code, exchangeRate.to.code)] =
+  final exchangeMap = <CodePair, ExchangeRate>{};
+
+  void register(ExchangeRate exchangeRate) {
+    exchangeMap[_generate(
+            exchangeRate.fromCurrency.code, exchangeRate.toCurrency.code)] =
         exchangeRate;
   }
 
-  Money exchangeTo(Money from, Code to) {
+  // void registerExchangeRate(ExchangeRateMap exchangeRate) {
+  //   exchangeMap[_generate(exchangeRate.from.code, exchangeRate.to.code)] =
+  //       exchangeRate;
+  // }
+
+  Money exchangeTo(Money from, CurrencyCode to) {
     var exchangeRate = exchangeMap[_generate(from.currency.code, to)];
 
     if (exchangeRate != null) {
@@ -30,19 +46,19 @@ class ExchangePlatform {
     throw UnknownExchangeRateException(from.currency.code, to);
   }
 
-  CodePair _generate(Code from, Code to) {
+  CodePair _generate(CurrencyCode from, CurrencyCode to) {
     return '$from:$to';
   }
 }
 
 /// Thrown if an attempt is made to calcuate the value of a [Money] amount
 /// in another currency for which there isn't a registered exchange rate.
-class UnknownExchangeRateException implements Exception {
+class UnknownExchangeRateException implements MoneyException {
   /// The from currency code in the unknown exchange
-  Code from;
+  CurrencyCode from;
 
   /// The to  currency code in the unknown exchange
-  Code to;
+  CurrencyCode to;
 
   ///
   UnknownExchangeRateException(this.from, this.to);
