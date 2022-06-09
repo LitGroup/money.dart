f0bfe9660a2f35d0bd209b4ff6013a7f74834421
Brett Sutton <bsutton@noojee.com.au>
2022-06-09 12:43:14 +1000

    added copyright notices.

--- example/example.dart
+++ example/example.dart
@@ -1,3 +1,8 @@
+/* Copyright (C) S. Brett Sutton - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
+ */
 import 'package:decimal/decimal.dart';
 import 'package:money2/money2.dart';
 // ignore_for_file: avoid_print
--- lib/src/exchange_rates/exchange_platform.dart
+++ lib/src/exchange_rates/exchange_platform.dart
@@ -1,3 +1,9 @@
+/* Copyright (C) S. Brett Sutton - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
+ */
+
 import '../money.dart';
 import 'exchange_rate.dart';
 
